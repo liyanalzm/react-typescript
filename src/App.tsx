d84d8ae058cc208fec71f3d654a00f93c9ddfab6
Nur Liyana Mohd Lazim <liyana@goquo.com>
2020-02-18 18:44:55 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './styles/index.css';
+
+class App extends React.PureComponent {
+	render() {
+		return (
+			<div>
+				<h1>Creating a react - typescript from scratch skrtt</h1>
+			</div>
+		);
+	}
+}
+
+export default App;
