9d4bde4a786b001c100b2d3e50a13eb01d468ffa
Liyana <liyana@wego.com>
2020-02-19 17:52:07 +0800

    Remove unuse router items

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import {
-  BrowserRouter as Router,
-  Switch,
-  useLocation
+  BrowserRouter as Router
 } from "react-router-dom";
 
 import styles from './styles/index.module.scss';
